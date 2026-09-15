3e35c7878777ca6ff13717e28f9b307a54c21fde
Prathyush-Varrey <prathyush.webdev@gmail.com>
2026-09-15 20:47:58 +0530

    Added come code in lists.py

--- python/Data-Structures/lists.py
+++ python/Data-Structures/lists.py
@@ -13,9 +13,37 @@ a = list()
 
 my_list = [1,2,3,4,5]
 
-print(my_list)
+#print(my_list)
 
 #list indexing -> 0 to length -1 len of the ds is going to be num of values in the ds
 
-print(my_list[0])
-print(my_list[len(my_list)-2])
+#print(my_list[0])
+#print(my_list[len(my_list)-2])
+
+lists = ["mango", "apple",'banana', 'guava','grapes','pineapple']
+
+#print(lists[5])
+#print(lists[2])
+
+#negative indexing
+#print(lists[-1])
+
+# Slincing -> list_name[start_index: stop_index : step]
+#print(lists[0:])
+
+# reverse order
+#print(lists[::-1])
+
+#changing data using indexing
+lists[4] = "papaya"
+print()
+#print(lists)
+
+#iterating through loops
+
+for items in lists:
+    print(items)
+
+nums = [1,2,3,4,5,6,7,8,9]
+
+print(nums[::1])
