02c4b604bf9115b8c07362e25176d3372ebd1e59
Prathyush-Varrey <prathyush.webdev@gmail.com>
2026-09-21 22:01:06 +0530

    Added some exampeles in functions.py file

--- python/Functions/functions.py
+++ python/Functions/functions.py
@@ -37,7 +37,45 @@ def sum_of_two_nums(num1, num2):
     sum = num1 + num2
     return f"sum of {num1} + {num2} = {sum}"
 
-num1 = int(input("Enter Number 1: "))
-num2 = int(input("Enter Number 2: "))
-results = sum_of_two_nums(num1, num2)
-print(results)
+#num1 = int(input("Enter Number 1: "))
+#num2 = int(input("Enter Number 2: "))
+#results = sum_of_two_nums(num1, num2)
+#print(results)
+
+# functions are reuseable and by that we mean to say is we don't have to write same piece of code again and again 
+
+
+def greetings(candidate_name):
+    ''' This function takes the input as the user name and provides the output
+        as greeting
+    '''
+    print(f"Hello {candidate_name}, How are you ? we really liked ur previous experience and ur skillset and now we are planning with but before that lets know about you")
+
+
+greetings("Amith")
+
+# take input from user (an integer) write a function to check weather given number is even or odd
+
+def check_if_even_or_odd(number):
+
+    if number%2 == 0:
+        return f"The Number {number} is Even"
+    else:
+        return f"The Number {number} is Odd"
+
+#user_num_input = int(input("Enter A Number: "))
+#result = check_if_even_or_odd(user_num_input)
+#print(result)
+
+def check_if_even_or_odd_instance(num):
+    if isinstance(num, int):
+        if num % 2 ==0:
+             return f"The Number {num} is Even"
+        else:
+             return f"The Number {num} is Odd"
+    else:
+        return f"{num} is a invalid value it should be number"
+
+num = int(input("Enter A Number : "))
+res  = check_if_even_or_odd_instance(num)
+print(res)
